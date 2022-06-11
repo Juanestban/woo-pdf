fce1e3bb76774faee77641f2e216e84d0e0a3a52
Juanestban <juanestban000@gmail.com>
2022-06-10 21:17:03 -0500

    fix: pwa disabled just when be in development node env

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
-const withServiceWorker = require('next-service-worker');
-
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
 
-module.exports = withServiceWorker({
+const production = process.env.NODE_ENV === 'production';
+
+module.exports = withPWA({
   reactStrictMode: true,
-  serviceWorker: {
-    // service worker config here...
-
-    workbox: {
-      // workbox config here...
-    },
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+    disable: !production,
   },
 });
