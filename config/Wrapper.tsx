978f20aaab8bf0606fdf097b1ce1ca0a9ff30526
Juanestban <juanestban000@gmail.com>
2022-08-15 00:17:04 -0500

    feat: add icon svg like component and context for handle the folders

--- config/Wrapper.tsx
+++ config/Wrapper.tsx
@@ -1,12 +1,18 @@
 import { FC, PropsWithChildren } from 'react';
-import { ThemeProvider, PdfViewProvider } from '@woo-pdf/contexts';
+import {
+  ThemeProvider,
+  PdfViewProvider,
+  FolderProvider,
+} from '@woo-pdf/contexts';
 
 type WrapperProps = PropsWithChildren;
 
 export const Wrapper: FC<WrapperProps> = ({ children }) => {
   return (
     <ThemeProvider>
-      <PdfViewProvider>{children}</PdfViewProvider>
+      <FolderProvider>
+        <PdfViewProvider>{children}</PdfViewProvider>
+      </FolderProvider>
     </ThemeProvider>
   );
 };
